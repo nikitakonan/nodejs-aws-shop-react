7b9cba34468668c71626f79897340ad45e2c3fb9
nikitakonan <nikitakonan1991@gmail.com>
2025-02-16 18:26:39 +0400

    Fixed deployment using Q

--- infrastructure/lib/infrastructure-stack.ts
+++ infrastructure/lib/infrastructure-stack.ts
@@ -1,9 +1,9 @@
 import * as cdk from 'aws-cdk-lib';
-import * as s3 from 'aws-cdk-lib/aws-s3';
 import * as cloudfront from 'aws-cdk-lib/aws-cloudfront';
-import { Construct } from 'constructs';
 import * as origins from 'aws-cdk-lib/aws-cloudfront-origins';
+import * as s3 from 'aws-cdk-lib/aws-s3';
 import * as s3Deployment from 'aws-cdk-lib/aws-s3-deployment';
+import { Construct } from 'constructs';
 
 const BUCKET_NAME = 'nikitakonan-test-web-app';
 const DISTRIBUTION_NAME = 'MyReactAppDistribution';
@@ -13,21 +13,46 @@ export class InfrastructureStack extends cdk.Stack {
     super(scope, id, props);
 
     const bucket = new s3.Bucket(this, BUCKET_NAME, {
-      websiteIndexDocument: 'index.html',
-      publicReadAccess: true,
-      blockPublicAccess: s3.BlockPublicAccess.BLOCK_ACLS,
+      blockPublicAccess: s3.BlockPublicAccess.BLOCK_ALL,
+      encryption: s3.BucketEncryption.S3_MANAGED,
+      enforceSSL: true,
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
     });
 
+    const originAccessIdentity = new cloudfront.OriginAccessIdentity(
+      this,
+      'MyOriginAccessIdentity'
+    );
+    bucket.grantRead(originAccessIdentity);
+
     const distribution = new cloudfront.Distribution(this, DISTRIBUTION_NAME, {
+      defaultRootObject: 'index.html',
       defaultBehavior: {
-        origin: origins.S3BucketOrigin.withBucketDefaults(bucket),
+        origin: origins.S3BucketOrigin.withOriginAccessIdentity(bucket, {
+          originAccessIdentity,
+        }),
+        viewerProtocolPolicy: cloudfront.ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
+        cachePolicy: cloudfront.CachePolicy.CACHING_OPTIMIZED,
       },
+      errorResponses: [
+        {
+          httpStatus: 404,
+          responseHttpStatus: 200,
+          responsePagePath: '/index.html',
+        },
+        {
+          httpStatus: 403,
+          responseHttpStatus: 200,
+          responsePagePath: '/index.html',
+        },
+      ],
     });
 
     new s3Deployment.BucketDeployment(this, 'DeployApp', {
       sources: [s3Deployment.Source.asset('../dist')],
       destinationBucket: bucket,
       distribution,
+      distributionPaths: ['/*'],
     });
   }
 }
