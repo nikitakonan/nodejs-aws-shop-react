5cb5aa67d8b6ae6919595d8be182855ab5445377
nikitakonan <nikitakonan1991@gmail.com>
2025-04-06 11:37:30 +0400

    Updated api paths

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -4,7 +4,7 @@ const API_PATHS = {
     "https://n70yzg0isg.execute-api.us-east-1.amazonaws.com/prod/api/profile/cart",
   import: "https://1a0lfhhyie.execute-api.us-east-1.amazonaws.com/prod",
   bff: "https://n70yzg0isg.execute-api.us-east-1.amazonaws.com/prod",
-  cart: "http://localhost:4000/api",
+  cart: "https://n70yzg0isg.execute-api.us-east-1.amazonaws.com/prod/api",
 };
 
 export default API_PATHS;
