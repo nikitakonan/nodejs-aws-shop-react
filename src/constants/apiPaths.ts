a2791df1fc6858a8089fb87a46e5daa3aeff6649
nikitakonan <nikitakonan1991@gmail.com>
2025-04-15 22:29:05 +0400

    Updated api paths

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,10 +1,10 @@
 const API_PATHS = {
   product: "https://34yloy2n19.execute-api.us-east-1.amazonaws.com/prod",
   order:
-    "https://n70yzg0isg.execute-api.us-east-1.amazonaws.com/prod/api/profile/cart",
+    "http://nikitakonan-cart-api-test.us-east-1.elasticbeanstalk.com/api/profile/cart",
   import: "https://1a0lfhhyie.execute-api.us-east-1.amazonaws.com/prod",
   bff: "https://n70yzg0isg.execute-api.us-east-1.amazonaws.com/prod",
-  cart: "https://n70yzg0isg.execute-api.us-east-1.amazonaws.com/prod/api",
+  cart: "http://nikitakonan-cart-api-test.us-east-1.elasticbeanstalk.com/api",
 };
 
 export default API_PATHS;
